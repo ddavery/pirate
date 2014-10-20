b2e003fcd82739d58836cb297e83e1c51a3af1d8
SupaDave <ddavery@gmail.com>
2014-10-20 08:58:58 -0400

    Updated Version

--- bartender.py
+++ bartender.py
@@ -1,43 +1,38 @@
 import random
 questions = {
-    "strong": "Do ye like yer drinks strong?",
-    "salty": "Do ye like it with a salty tang?",
-    "bitter": "Are ye a lubber who likes it bitter?",
-    "sweet": "Would ye like a bit of sweetness with yer poison?",
-    "fruity": "Are ye one for a fruity finish?"
+	"strong": "Do ye like yer drinks strong?",
+	"salty": "Do ye like it with a salty tang?",
+	"bitter": "Are ye a lubber who likes it bitter?",
+	"sweet": "Would ye like a bit of sweetness with yer poison?",
+	"fruity": "Are ye one for a fruity finish?"
 }
 ingredients = {
-    "strong": ["glug of rum", "slug of whisky", "splash of gin"],
-    "salty": ["olive on a stick", "salt-dusted rim", "rasher of bacon"],
-    "bitter": ["shake of bitters", "splash of tonic", "twist of lemon peel"],
-    "sweet": ["sugar cube", "spoonful of honey", "spash of cola"],
-    "fruity": ["slice of orange", "dash of cassis", "cherry on top"]
-}
-answer_quest = {
-    "strong":[],
-    "salty" :[],
-    "bitter":[],
-    "sweet" :[],
-    "fruity":[],
+	"strong": ["glug of rum", "slug of whisky", "splash of gin"],
+	"salty": ["olive on a stick", "salt-dusted rim", "rasher of bacon"],
+	"bitter": ["shake of bitters", "splash of tonic", "twist of lemon peel"],
+	"sweet": ["sugar cube", "spoonful of honey", "spash of cola"],
+	"fruity": ["slice of orange", "dash of cassis", "cherry on top"]
 }
 def answer_questions():
-    for key in questions:
-     print key,questions[key]
-     answers = raw_input("Please enter in a yes or y or no n if you like the drink: ")
-     if answers == 'yes' or answers == 'y':
-       answer_quest[key] = True
-     elif answers == 'no' or answers == 'n':
-       answer_quest[key] = False
-     else:
-        print 'Please enter yes or no in lower case'
-    return answer_quest
-answer_questions()
-for d in answer_quest:
-  print d, answer_quest[d]
-def drinks(answer_questions):
-    shot = []
-    for k in answer_quest:
-        if answer_quest.keys() == ingredients.keys() and answer_quest[k] == True:
-          shot.append(ingredients[k])
-    print random.choice(shot)
+	answer_quest = {}
+	for key in questions:
+		print key,questions[key]
+		answers = raw_input("Please enter in a yes or y or no n if you like the drink: ")
+		if answers == 'yes' or answers == 'y':
+			answer_quest[key] = questions[key]
+			answer_quest[key] = True
+		elif answers == 'no' or answers == 'n':
+		    answer_quest[key] = questions[key]
+		    answer_quest[key] = False
+		else:
+			print 'Please enter yes or no in lower case'
+	answer_quest = answer_questions()
+	return answer_quest
+drinks1 = answer_questions()
+def drinks(drinks1):
+	shot = []
+	for k in drinks1:
+		if drinks1.keys() == ingredients.keys() and drinks1[k]:
+		  shot.append(ingredients[k])
+	print random.choice(shot)
 drinks(answer_questions)
