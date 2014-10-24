8694a2ff89191e43f00ffa5de73ccbea818901e3
SupaDave <ddavery@gmail.com>
2014-10-23 22:57:59 -0400

    Updated object program

--- testbi.py
+++ testbi.py
@@ -2,33 +2,32 @@ class Bicycle(object):
 	def __init__(self,bike):
 		self.bike = bike
 	def create_bikes(self):
-		if self.bike:
-			bikes = {
-	        "BMX1000": [10, 500],
-	        "SPX1000": [20, 20],
-	        "DMX1000": [15, 300],
-	        "LMX1000": [30, 100],
-	        "PMX1000": [40, 50],
-	        "JMX1000": [35, 25],
-	        "MMX1000": [5, 1000],
-	        "MX1000": [10, 100],
-	        "SMX1000": [30, 150],
-	        "ZMX1000": [25, 350],
-	        }
-	        return bikes
-class Bike_Shops(Bicycle):
-	def __int__(self,name):
-		self.name = name
-	def get_bike_inv(create_bikes):
-		bi_inv = {}
+	    global bikes
+	    bikes = {}
+	    for i in range(7):
+	    	m = raw_input("Please input a model: ")
+	    	w = raw_input("Please input a weight for the bike: ")
+	    	c = raw_input("Please input a cost")
+	    	if bikes.has_key(m):
+	    		print 'Please input a different model'
+	    	else:
+	    		bikes[m] = w
+	    		bikes[m] = c
+	    return bikes
+	def bike_inv(create_bikes):
 		for key in bikes:
-			if bike:
-				bi_inv[key] = bikes[key]
-				bi_inv[key] = bikes[key] 
-				bi_inv[key] = bikes[key] * .20 + bikes[key]
-		print bi_inv
-test1 = Bicycle(True)
-test2 = Bike_Shops("Daves Bike Shop")
-test2.get_bike_inv()
-
-
+		  print key,bikes[key]
+class Bike_Shop(Bicycle):
+	def __init__(self,name):
+		self.name = name
+	def bike_inv2(bikes):
+		bikes2 = {}
+		for key in range(5):
+			bikes2[key] = bikes[key]
+    		bikes2[key] = .20
+        return bikes2
+test1 = Bicycle("Daves Bike Shop")
+test1.create_bikes()
+test1.bike_inv()
+test2 = Bike_Shop("Daves Place")
+test2.bike_inv2()
